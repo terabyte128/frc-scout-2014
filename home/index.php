8c5f3968c64cbd9bcf989565511f57ab3b83ee66
Sam <terabyte128@gmail.com>
2013-11-09 18:19:01 -0800

    Removed photo upload compatibility.

--- home/index.php
+++ home/index.php
@@ -20,21 +20,9 @@ require '../includes/setup-session.php';
                     <br>
                     <font style="color: #bbb; float: right; font-size: 10pt;">Admin Tools</font>
                     <hr style="border-top: 1px solid #bbb">
-                    <button onclick="$('#fileUploadWrapper').show();" class="btn btn-lg btn-info btn-home-selections">Change Team Picture</button>
                     <button onclick="window.location = 'change-password.php';" class="btn btn-lg btn-warning btn-home-selections">Change Team Password</button>
                     <button onclick="window.location = 'change-admin-password.php';" class="btn btn-lg btn-warning btn-home-selections">Change Admin Password</button>
-                    <div style="display: none;" id="fileUploadWrapper">
-                        <span class="btn btn-success fileinput-button">
-                            <i class="glyphicon glyphicon-plus"></i>
-                            <span>Select files...</span>
-                            <!-- The file input field used as target for the file upload widget -->
-                            <input id="fileupload" type="file" name="files[]" data-url="team-photos">
-                        </span>
-                        <div id="progress" class="progress">
-                            <div class="progress-bar progress-bar-success" style="width: 0%;"></div>
-                        </div>
-                        <div id="files" class="files"></div>
-                    </div>
+                    
                 <?php } ?>
 
                 <br />
@@ -61,26 +49,6 @@ require '../includes/setup-session.php';
         </div>
 
         <script type="text/javascript">
-
-                    $(function() {
-                        $('#fileupload').fileupload({
-                            dataType: 'json',
-                            done: function(e, data) {
-                                $.each(data.result.files, function(index, file) {
-                                    $('<p/>').text(file.name).appendTo('#files');
-                                });
-                            },
-                            progressall: function(e, data) {
-                                var progress = parseInt(data.loaded / data.total * 100, 10);
-                                $('#progress .progress-bar').css(
-                                        'width',
-                                        progress + '%'
-                                        );
-                            }
-                        }).prop('disabled', !$.support.fileInput)
-                                .parent().addClass($.support.fileInput ? undefined : 'disabled');
-                    });
-
                     function loginAdmin() {
                         $("#authButton").button('loading');
                         var adminPassword = $("#adminPassword").val();
