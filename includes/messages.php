c10f38dffd8453ffb45788986df1171c0f9a9d14
Sam Wolfson <terabyte128@gmail.com>
2013-11-09 16:17:40 -0800

    Made messages.php always use the functions provided by messages.js for showing messages passed via localStorage.

--- includes/messages.php
+++ includes/messages.php
@@ -1,5 +1,5 @@
     <div style="margin-top: 10px; padding: 10px 10px 10px 10px; display: none;" id="inputError">
-        <button type="button" class="close" onclick="$('.alert').slideUp(250);">&times;</button>
+        <button type="button" class="close" onclick="hideMessage();">&times;</button>
         <strong id='alertError'></strong>
     </div>
 <script type="text/javascript">
@@ -7,11 +7,9 @@
         $(function() {
             
             if (localStorage.message !== undefined) {
-                $('#alertError').html(localStorage.message);
-                $('#inputError').attr("class","alert alert-" + localStorage.type);
+                showMessage(localStorage.message, localStorage.type);
                 delete(localStorage.message);
                 delete(localStorage.type);
-                $('#inputError').slideDown(250);
             }
         });
 
