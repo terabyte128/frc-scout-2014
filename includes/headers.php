fe72e4ff441dbb7801e08ebd3aec8e367b5d3614
Sam <terabyte128@gmail.com>
2013-11-09 18:15:40 -0800

    Adding photo upload capability.

--- includes/headers.php
+++ includes/headers.php
@@ -10,8 +10,12 @@
 <!-- js -->
 <script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="/includes/message-control.js"></script>
+<script type="text/javascript" src="/file-upload/js/vendor/jquery.ui.widget.js"></script>
+<script type="text/javascript" src="/file-upload/js/jquery.iframe-transport.js"></script>
+<script type="text/javascript" src="/file-upload/js/jquery.fileupload.js"></script>
 
 <!-- styles 'n fonts -->
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" type='text/css' />
 <link rel="stylesheet" href="/css/style.css" type='text/css' />
 <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Cabin' type='text/css' />
+<link rel="stylesheet" href="/file-upload/css/jquery.fileupload.css" />
