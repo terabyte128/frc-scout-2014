8d7b093d4a8cc25515bb597253e43c97525fea63
Sam <terabyte128@gmail.com>
2014-02-12 08:04:31 -0800

    Added missed goals to autonomous.

--- home/scouting/match/forms/autonomous.php
+++ home/scouting/match/forms/autonomous.php
@@ -2,118 +2,129 @@
     <label for='scoredGoal'>Scored goal:</label>
     <br />
     <div class="btn-group" data-toggle="buttons" id='scoredGoal'>
-        <label class="btn btn-danger btn-lg" id="0" onclick='goalValue = 0;
-                updateTotals();'>
+        <label class="btn btn-warning btn-lg" id="0" onclick='goalValue = 0;
+                updateTotals(); updateMissed(false);'>
             <input type="radio" name="options" id="noGoal">None
         </label>
+        <label class="btn btn-danger btn-lg" id="0" onclick='goalValue = 0;
+                updateTotals(); updateMissed(true);'>
+            <input type="radio" name="options" id="missedGoal">Missed
+        </label>
         <label class="btn btn-blue-selection btn-lg" id="6" onclick='goalValue = 6;
-                updateTotals();'>
+                updateTotals(); updateMissed(false);'>
             <input type="radio" name="options" id="lowGoal">Low
         </label>
         <label class="btn btn-success btn-lg" id="15" onclick='goalValue = 15;
-                updateTotals();'>
+                updateTotals(); updateMissed(false);'>
             <input type="radio" name="options" id="highGoal">High
         </label>
     </div>
     <br />
     <button data-toggle="button" class="btn btn-lg btn-warning" id="hotGoal" style='margin-top: 10px; width: 212px;' onclick='updateHotGoal();
-            updateTotals();' disabled>Hot Goal</button>
+                updateTotals();' disabled>Hot Goal</button>
     <br />
     <br />
     <button data-toggle="button" class="btn btn-lg btn-info btn-no-border" id="movedToAllianceZone" style='margin-top: 10px; width: 212px;' onclick='updateMovedToAllianceZone();
-            updateTotals();'>Moved to <span id='allianceColor'></span> zone</button>
+                updateTotals();'>Moved to <span id='allianceColor'></span> zone</button>
     <br />
     <br />
     <h4><strong>Total Points: <span id='totalPoints'>0</span></strong></h4>
 
 </form>
 <script type="text/javascript">
-    $(function() {
-        $("#pageNameTitle").text("Autonomous");
-        $("#allianceColor").text(localStorage.allianceColor);
-        $("#movedToAllianceZone").css({
-            "background-color": localStorage.allianceColorId,
-            "text-color": "white"
-        });
-        //document.location.hash = "autonomous";
+            $(function() {
+                $("#pageNameTitle").text("Autonomous");
+                $("#allianceColor").text(localStorage.allianceColor);
+                $("#movedToAllianceZone").css({
+                    "background-color": localStorage.allianceColorId,
+                    "text-color": "white"
+                });
+                //document.location.hash = "autonomous";
 
-    });
-
-    var goalValue = undefined;
-
-    var hotGoal = false;
-    var movedToAllianceZone = false;
-
-    function updateHotGoal() {
-        hotGoal = !hotGoal;
-    }
-
-    function updateMovedToAllianceZone() {
-        movedToAllianceZone = !movedToAllianceZone;
-    }
-
-    function updateTotals() {
-        totalPoints = 0;
-
-        if (goalValue !== undefined) {
-            totalPoints += goalValue;
-        }
-
-        if (hotGoal) {
-            totalPoints += 5;
-        }
-
-        if (movedToAllianceZone) {
-            totalPoints += 5;
-        }
-
-        $("#totalPoints").text(totalPoints);
-        if (goalValue !== 0 && goalValue !== undefined) {
-            $("#hotGoal").prop("disabled", false);
-        } else {
-            if (hotGoal) {
-                $("#hotGoal").click();
-                $("#hotGoal").removeClass("active");
-            }
-            $("#hotGoal").prop("disabled", true);
-        }
-    }
-
-    function pushToLocalStorage() {
-        if (!(goalValue === undefined)) {
-            localStorage.autoGoalValue = goalValue;
-            localStorage.autoHotGoal = hotGoal;
-            localStorage.autoMovedToAllianceZone = movedToAllianceZone;
-            hideMessage();
-            //nextPhase();
-            changePhase("teleoperated");
-
-        } else {
-            showMessage("Please select goal scored.", "danger");
-        }
-    }
-
-    function pullFromLocalStorage() {
-        if (localStorage.autoGoalValue !== undefined) {
-            goalValue = parseInt(localStorage.autoGoalValue);
-            $("#" + goalValue).addClass("active");
-        }
-
-        if (localStorage.autoHotGoal === "true") {
-            updateHotGoal();
-            $("#hotGoal").addClass("active");
-        }
-
-        if (localStorage.autoMovedToAllianceZone === "true") {
-            updateMovedToAllianceZone();
-            $("#movedToAllianceZone").addClass("active");
-            
-            $("#movedToAllianceZone").css({
-                "background-color": localStorage.allianceColorId,
-                "text-color": "white"
             });
-        }
 
-        updateTotals();
-    }
+            var goalValue = undefined;
+            
+            var missedGoal = false;
+            
+            function updateMissed(missed) {
+                missedGoal = missed;
+            }
+
+            var hotGoal = false;
+            var movedToAllianceZone = false;
+
+            function updateHotGoal() {
+                hotGoal = !hotGoal;
+            }
+
+            function updateMovedToAllianceZone() {
+                movedToAllianceZone = !movedToAllianceZone;
+            }
+
+            function updateTotals() {
+                totalPoints = 0;
+
+                if (goalValue !== undefined) {
+                    totalPoints += goalValue;
+                }
+
+                if (hotGoal) {
+                    totalPoints += 5;
+                }
+
+                if (movedToAllianceZone) {
+                    totalPoints += 5;
+                }
+
+                $("#totalPoints").text(totalPoints);
+                if (goalValue !== 0 && goalValue !== undefined) {
+                    $("#hotGoal").prop("disabled", false);
+                } else {
+                    if (hotGoal) {
+                        $("#hotGoal").click();
+                        $("#hotGoal").removeClass("active");
+                    }
+                    $("#hotGoal").prop("disabled", true);
+                }
+            }
+
+            function pushToLocalStorage() {
+                if (!(goalValue === undefined)) {
+                    localStorage.autoGoalValue = goalValue;
+                    localStorage.autoHotGoal = hotGoal;
+                    localStorage.autoMovedToAllianceZone = movedToAllianceZone;
+                    localStorage.autoMissedGoal = missedGoal;
+                    hideMessage();
+                    //nextPhase();
+                    changePhase("teleoperated");
+
+                } else {
+                    showMessage("Please select goal scored.", "danger");
+                }
+            }
+
+            function pullFromLocalStorage() {
+                if (localStorage.autoGoalValue !== undefined) {
+                    goalValue = parseInt(localStorage.autoGoalValue);
+                    $("#" + goalValue).addClass("active");
+                }
+
+                if (localStorage.autoHotGoal === "true") {
+                    updateHotGoal();
+                    $("#hotGoal").addClass("active");
+                }
+
+                if (localStorage.autoMovedToAllianceZone === "true") {
+                    updateMovedToAllianceZone();
+                    $("#movedToAllianceZone").addClass("active");
+
+                    $("#movedToAllianceZone").css({
+                        "background-color": localStorage.allianceColorId,
+                        "text-color": "white"
+                    });
+                }
+
+                updateTotals();
+            }
 </script>
