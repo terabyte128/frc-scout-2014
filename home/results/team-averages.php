f0e6f2bfb6de955368d3368d0fd4454fc6a5f759
Sam <terabyte128@gmail.com>
2014-02-12 23:29:02 -0800

    Made averages default to "everywhere"

--- home/results/team-averages.php
+++ home/results/team-averages.php
@@ -17,7 +17,8 @@
                 <h2>Team Averages</h2>
                 <button class="btn btn-default" onclick="window.location = '/'" style="margin-bottom: 10px;">Return Home</button>
                 <br />
-                <a href="#" onclick="$('#filterOptions').slideToggle(200); return false;">Filter these results</a>
+                <a href="#" onclick="$('#filterOptions').slideToggle(200);
+                        return false;">Filter these results</a>
                 <div id="filterOptions" style="display:none;">
                     <label>View data collected by:</label><br />
                     <div class="btn-group" data-toggle="buttons" id="matchOutcome">
@@ -30,11 +31,11 @@
                     </div><br /><br />
                     <div class="btn-group" data-toggle="buttons" id="matchOutcome">
                         <label>View data from:</label><br />
-                        <label class="btn btn-default active" style="width: 130px;" id="here" onclick="setFilterHash('global', 'here');">
-                            <input type="radio">Here<!--<?php echo $location; ?>-->
-                        </label>
-                        <label class="btn btn-default" style="width: 130px;" id="global" onclick="setFilterHash('here', 'global');">
+                        <label class="btn btn-default active" style="width: 130px;" id="global" onclick="setFilterHash('here', 'global');">
                             <input type="radio">Everywhere
+                        </label>
+                        <label class="btn btn-default" style="width: 130px;" id="here" onclick="setFilterHash('global', 'here');">
+                            <input type="radio">Here<!--<?php echo $location; ?>-->
                         </label>
                     </div>
                     <br />
@@ -60,69 +61,69 @@
             </div>
         </div>
         <script type="text/javascript">
-            $(function() {
-                if (window.location.hash.indexOf("only") !== -1) {
-                    onlyUs = true;
-                    $("#only").addClass("active");
-                    $("#all").removeClass("active");
-                } else {
-                    onlyUs = false;
-                }
-                if (window.location.hash.indexOf("global") !== -1) {
-                    onlyHere = false;
-                    $("#global").addClass("active");
-                    $("#here").removeClass("active");
-                } else {
-                    onlyHere = true;
-                }
-                loadTable(onlyUs, onlyHere);
-                $("#averagesTable").tablesorter({
-                    sortForce: [[1, 1]]
-                });
-            });
+                    $(function() {
+                        if (window.location.hash.indexOf("only") !== -1) {
+                            onlyUs = true;
+                            $("#only").addClass("active");
+                            $("#all").removeClass("active");
+                        } else {
+                            onlyUs = false;
+                        }
+                        if (window.location.hash.indexOf("global") === -1) {
+                            onlyHere = true;
+                            $("#global").removeClass("active");
+                            $("#here").addClass("active");
+                        } else {
+                            onlyHere = false;
+                        }
+                        loadTable(onlyUs, onlyHere);
+                        $("#averagesTable").tablesorter({
+                            sortForce: [[1, 1]]
+                        });
+                    });
 
-            var setFilterHash = function(thingToChange, thingToChangeTo) {
-                if (window.location.hash.indexOf(thingToChange) !== -1) {
-                    window.location.hash = window.location.hash.replace(thingToChange, thingToChangeTo);
-                } else if (window.location.hash.indexOf(thingToChangeTo) === -1) {
-                    if (window.location.hash === "") {
-                        window.location.hash = thingToChangeTo;
-                    } else {
-                        window.location.hash = window.location.hash + "," + thingToChangeTo;
+                    var setFilterHash = function(thingToChange, thingToChangeTo) {
+                        if (window.location.hash.indexOf(thingToChange) !== -1) {
+                            window.location.hash = window.location.hash.replace(thingToChange, thingToChangeTo);
+                        } else if (window.location.hash.indexOf(thingToChangeTo) === -1) {
+                            if (window.location.hash === "") {
+                                window.location.hash = thingToChangeTo;
+                            } else {
+                                window.location.hash = window.location.hash + "," + thingToChangeTo;
+                            }
+                        }
+                    };
+
+                    window.onhashchange = function() {
+                        if (window.location.hash.indexOf("only") !== -1) {
+                            onlyUs = true;
+                        } else {
+                            onlyUs = false;
+                        }
+                        if (window.location.hash.indexOf("global") !== -1) {
+                            onlyHere = false;
+                        } else {
+                            onlyHere = true;
+                        }
+                        loadTable(onlyUs, onlyHere);
+                    };
+
+
+                    function loadTable(onlyLoggedInTeam, onlyThisLocation) {
+                        $.ajax({
+                            url: '/ajax-handlers/load-frc-team-averages.php',
+                            data: {
+                                'onlyLoggedInTeam': onlyLoggedInTeam,
+                                'onlyThisLocation': onlyThisLocation
+                            },
+                            success: function(response) {
+                                $("#tableBody").html(response);
+                                $("#averagesTable").trigger("update");
+                                var sorting = [[1, 1]];
+                                $("#averagesTable").trigger("sorton", [sorting]);
+                            }
+                        });
                     }
-                }
-            };
-
-            window.onhashchange = function() {
-                if (window.location.hash.indexOf("only") !== -1) {
-                    onlyUs = true;
-                } else {
-                    onlyUs = false;
-                }
-                if (window.location.hash.indexOf("global") !== -1) {
-                    onlyHere = false;
-                } else {
-                    onlyHere = true;
-                }
-                loadTable(onlyUs, onlyHere);
-            };
-
-
-            function loadTable(onlyLoggedInTeam, onlyThisLocation) {
-                $.ajax({
-                    url: '/ajax-handlers/load-frc-team-averages.php',
-                    data: {
-                        'onlyLoggedInTeam': onlyLoggedInTeam,
-                        'onlyThisLocation': onlyThisLocation
-                    },
-                    success: function(response) {
-                        $("#tableBody").html(response);
-                        $("#averagesTable").trigger("update");
-                        var sorting = [[1,1]];
-                        $("#averagesTable").trigger("sorton", [sorting]);
-                    }
-                });
-            }
         </script>
     </body>
 </html>
