c6cd450fb340770f7a8bb3d6777a2b3f18cb4708
webster <webster@WINGRM20603.seattleschools.org>
2014-02-28 08:50:20 -0800

    Changed release number.

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
                                     echo "<br><br><span style='color:firebrick;'>development </span>";
                                 };
                                 ?>
-                                v1.1.3
+                                v1.1.4
                             </strong>
                         </em>
                     </p>
