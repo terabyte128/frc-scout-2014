f3282909c714bece061695ae0435609ae20bcace
Sam Wolfson <terabyte128@gmail.com>
2014-03-21 17:11:33 -0700

    More detailed stats on login page cuz why not.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/redirect-if-session-exists.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/ajax-handlers/get-scouting-stats.php'; ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -56,7 +57,7 @@
                     </form> 
                     <br />
                     
-                    <p>So far, <?php include $_SERVER['DOCUMENT_ROOT'] . '/ajax-handlers/get-registered-teams.php'; ?> teams have registered on FRC Scout!</p>
+                    <p>So far, <strong><?php include $_SERVER['DOCUMENT_ROOT'] . '/ajax-handlers/get-registered-teams.php'; ?> teams</strong> have match scouted <strong><?= $scoutCount['matchScouted'] ?> times</strong> and pit scouted <strong><?= $scoutCount['pitScouted'] ?> times</strong>!</p>
                     
                     <p>
                         <em>
