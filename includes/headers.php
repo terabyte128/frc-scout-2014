b9db907f3224be2a7963a8cacfce672cd1627f71
Sam Wolfson <terabyte128@gmail.com>
2014-03-22 13:06:14 -0700

    Splash screen and other things for iDevices!

--- includes/headers.php
+++ includes/headers.php
@@ -1,11 +1,15 @@
 <!-- declare character encoding -->
 <meta charset="UTF-8">
-<meta name="mobile-web-app-capable" content="yes" />
 
 <!-- ensure mobile-friendliness -->
 <meta name="mobileoptimized" content="0" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+<meta name="mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-capable" content="yes">
 
+<link rel="shortcut icon" sizes="196x196" href="/images/icon.png">
+<link rel="apple-touch-icon" sizes="196x196" href="/images/icon.png">
+<link rel="apple-touch-startup-image" href="/images/splash.png">
 <!-- jquery -->
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 
@@ -17,7 +21,6 @@
 
 <!-- js stuff to run on page load -->
 <script type="text/javascript" src="/includes/run-on-page-load.js"></script>
-<script type="text/javascript" src="/includes/jquery-file-upload.js"></script>
 <script type="text/javascript" src="/includes/jquery.form.js"></script>
 <script type="text/javascript" src="/includes/typeahead.js"></script>
 
@@ -26,7 +29,8 @@
 
 <!-- styles 'n fonts -->
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" type='text/css' />
-<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Cabin' type='text/css' />
 <link rel="stylesheet" href="/bootstrap3-editable/css/bootstrap-editable.css" />
 <link rel="stylesheet" href="/css/style.css" type='text/css' />
 <link rel="stylesheet" href="/css/datepicker.css" type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
